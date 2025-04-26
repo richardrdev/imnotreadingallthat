102fb61f588cf9fd4590213c38b753ae0aeb2514
richardrdev <137314274+richardrdev@users.noreply.github.com>
2025-04-26 10:52:10 -0400

    test vue project added to frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <button @click="count++">
+            Count is: {{ count+100 }}
+        </button>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        return {
+            count: ref(0),
+        };
+    },
+};
+</script>
---
+++ frontend/src/index.js
@@ -0,0 +1,4 @@
+import { createApp, ref } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
